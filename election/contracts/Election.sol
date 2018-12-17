40b5a14cc0abe3ee18a3a625d8296810fde5fba4
Ganesh Patra <ganesh.patra@somaiya.edu>
2018-12-18 03:26:36 +0530

    finished

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -8,11 +8,18 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
     constructor() public {
         addCandidate("Candidate 1");
@@ -24,4 +31,20 @@ contract Election {
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateId);
+    }
 }
